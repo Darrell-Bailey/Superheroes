5c64676b3590428b01009d3423710ec500563467
Darrell Bailey <darrelljsbailey@gmail.com>
2025-03-06 11:10:10 -0500

    Created functions for custom hero cards in the UI including info regarding icons, names and descriptions.

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -3,6 +3,7 @@ package com.example.superheroes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
@@ -10,23 +11,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.superheroes.data.Hero
+import com.example.superheroes.data.HeroesRepository
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 @Composable
 fun SuperheroItem(
+    superhero: Hero,
     modifier: Modifier = Modifier
 ){
     Card (
         modifier = modifier
             .padding(dimensionResource(R.dimen.padding_medium))
-            .height(72.dp)
             .clip(MaterialTheme.shapes.medium),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 2.dp
@@ -34,11 +41,13 @@ fun SuperheroItem(
     ) {
         Row (
             modifier = Modifier
+                .height(104.dp)
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-            Column {
-
-            }
-
+            HeroInformation(heroName = superhero.nameRes, heroDescription = superhero.descriptionRes)
+            HeroIcon(heroIcon = superhero.imageRes, contentDesc = superhero.nameRes)
         }
     }
 }
@@ -46,24 +55,46 @@ fun SuperheroItem(
 @Composable
 fun HeroIcon(
     @DrawableRes heroIcon: Int,
-    modifier: Modifier
+    @StringRes contentDesc: Int,
+    modifier: Modifier = Modifier
 ){
-
+    Image(
+        painter = painterResource(heroIcon),
+        contentDescription = stringResource(contentDesc),
+        modifier = modifier
+            .clip(MaterialTheme.shapes.small)
+    )
 }
 
 @Composable
 fun HeroInformation(
     @StringRes heroName: Int,
     @StringRes heroDescription: Int,
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ){
-
+    Column (
+        modifier = modifier
+            .padding(end = dimensionResource(R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(heroName),
+            style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier
+        )
+        Text(
+            text = stringResource(heroDescription),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SuperheroItemPreview() {
     SuperheroesTheme {
-        SuperheroItem()
+        SuperheroItem(HeroesRepository.heroes[0])
     }
 }
--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.data.HeroesRepository
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,12 +37,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperheroesTheme {
-        Greeting("Android")
-    }
 }
